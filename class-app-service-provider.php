0b32a7bcedb9f30a979c3d5a925cf940d5162b26
mantle-ci <mantle@alley.com>
2023-01-06 03:29:10 +0000

    Merge pull request #357 from alleyinteractive/application-isolation

---
+++ class-app-service-provider.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * App_Service_Provider class file.
+ *
+ * @package Mantle
+ */
+
+namespace Mantle\Application;
+
+use Mantle\Contracts\Application;
+use Mantle\Scheduling\Schedule;
+use Mantle\Support\Service_Provider;
+
+use function Mantle\Support\Helpers\tap;
+
+/**
+ * App Service Provider
+ */
+abstract class App_Service_Provider extends Service_Provider {
+	/**
+	 * Application instance.
+	 *
+	 * @var Application
+	 */
+	protected $app;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param Application $app Application instance.
+	 */
+	public function __construct( Application $app ) {
+		$this->app = $app;
+
+		$this->app->booted(
+			fn () => $this->boot_scheduler(),
+		);
+	}
+
+	/**
+	 * Boot the scheduler service.
+	 */
+	protected function boot_scheduler() {
+		$this->app->singleton(
+			Schedule::class,
+			fn ( $app ) => tap(
+				new Schedule( $app ),
+				fn ( Schedule $schedule ) => $this->schedule( $schedule ),
+			),
+		);
+
+		Schedule::schedule_cron_event();
+	}
+
+	/**
+	 * Define the application's command schedule.
+	 *
+	 * @param Schedule $schedule Schedule instance.
+	 */
+	protected function schedule( $schedule ) { }
+}
---
+++ class-application-exception.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Application_Exception class file.
+ *
+ * @package Mantle
+ */
+
+namespace Mantle\Application;
+
+/**
+ * Application Exception
+ */
+class Application_Exception extends \Exception { }
